bf9749f86540dfc822f9fa1be681b36b5fd9238b
mrozk <mrozk2012@gmail.com>
2015-04-28 17:34:30 +0300

    Ответная часть по статистике

--- example/index.php
+++ example/index.php
@@ -4,21 +4,33 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
-<script src="js/ddelivery.js"></script>
+<script src="http://devsdk.ddelivery.ru/js/ddelivery.js?10"></script>
 
 <a href="javascript:void(0)" id="select_way" class="trigger">Выбрать точку доставки</a>
 
 <script>
+
+    var select_way = document.getElementById("select_way");
     DDeliveryIntegration.onOpen = function(){
-        alert("Мой вариант");
-    }
+        alert("Хук на открытие окна");
+    };
     DDeliveryIntegration.onChange = function(data){
-        alert("Цемики");
+        alert("Хук на окончание оформления заказа и обработка результата");
         console.log(data);
-    }
-    select_way = document.getElementById('select_way');
+    };
+    DDeliveryIntegration.onClose = function(data){
+        alert("Хук на закрытие окна");
+        console.log(data);
+    };
+    //var select_way = document.getElementById("select_way");
+    var params = {
+        url: 'ajax.php?action=shop',
+        width: 1000,
+        height: 650
+    };
+
     select_way.onclick = function(){
-        DDeliveryIntegration.openPopup({});
+        DDeliveryIntegration.openPopup(params);
     };
 </script>
 
